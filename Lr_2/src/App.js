45348524a38f858b93d2827e134630b412a29fc4
KrakenfromKSP <KrakenfromKSP@gmail.com>
2024-09-06 16:38:00 +0300

    Lr_2

---
+++ Lr_2/src/App.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import localData from './Data';
+// ATTENTION!!!!!!!!!!
+// Check URL
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setloading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const restTour = tours.filter((el) => el.id !== id);
+    setTours(restTour);
+  };
+
+  const fetchTours = async () => {
+    setloading(true);
+    try {
+      //const response = await fetch(url);
+      //const tours = await response.json();
+      const tours = localData;
+      console.log(tours);
+      setloading(false);
+      setTours(tours);
+    } catch (error) {
+      setloading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour}/>
+    </main>
+  );
+}
+
+export default App;
